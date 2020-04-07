19f637f2404453fbd5fcf15be8d4e1a1a06f153e
runningsparrow <runningsparrow@gmail.com>
2020-04-07 09:41:11 +0800

    main

--- main.py
+++ main.py
@@ -2,10 +2,7 @@
 from jd_test import Jd_test
 
 if __name__ == '__main__':
-    """
-    重要提示：此处为示例代码之一，请移步下面的链接查看使用教程👇
-    https://github.com/tychxn/jd-assistant/wiki/1.-%E4%BA%AC%E4%B8%9C%E6%8A%A2%E8%B4%AD%E5%8A%A9%E6%89%8B%E7%94%A8%E6%B3%95
-    """
+ 
 
     # sku_ids = '100001324422'  # 商品id
     sku_ids = '100005267656'  # 商品id
